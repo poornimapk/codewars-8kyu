55632eaca3089537b6db1bcdd515a597077b8b29
poornimapk <poornima.pk@gmail.com>
2022-04-06 12:30:56 -0700

    Removed every second element from the array

---
+++ removing-elements/js/main.js
@@ -0,0 +1,19 @@
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+    /* return arr.filter((element, index) => { return (index%2 === 0) ? true : false}); */
+
+    return arr.filter((element, index) => index%2 === 0);
+}
+
+// TEST CASES
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+console.log(removeEveryOther([[1, 2]]), [[1, 2]]);
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
